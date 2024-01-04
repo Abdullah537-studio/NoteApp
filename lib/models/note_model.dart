0492ca50f8ade18e0f49a6e1e69e82e964ace0f9
Abdullah537-studio <112731731+Abdullah537-studio@users.noreply.github.com>
2024-01-04 11:51:17 +0300

    generate type adapter

---
+++ lib/models/note_model.dart
@@ -0,0 +1,21 @@
+import 'package:hive/hive.dart';
+
+// فيك تقول انها نفس استعمال ال  import قريبة منها
+// هلق بدي حط اسم الكلاس note_modl بعدين .g مشان generate بعدين .dart
+// اسم ال file يلي بدي اعملو generate
+part 'note_model.g.dart';
+// بعدين بنحط flutter packages pub run build_runner build بال terminal
+
+@HiveType(typeId: 0)
+class NoteModels extends HiveObject {
+  @HiveField(0)
+  final String title;
+  @HiveField(1)
+  final String subtitle;
+  @HiveField(2)
+  final String date;
+  @HiveField(3)
+  final int color;
+  NoteModels(this.color,
+      {required this.title, required this.subtitle, required this.date});
+}
--- lib/models/note_models.dart
+++
@@ -1,8 +0,0 @@
-class NoteModels {
-  final String title;
-  final String subtitle;
-  final String date;
-  final int color;
-  NoteModels(this.color,
-      {required this.title, required this.subtitle, required this.date});
-}
