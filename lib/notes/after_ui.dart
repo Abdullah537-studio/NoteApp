1e407b4fe08387ecf05c2974c8c4f9403cd0f0be
Abdullah537-studio <112731731+Abdullah537-studio@users.noreply.github.com>
2024-01-04 11:20:36 +0300

    add hive

--- lib/notes/after_ui.dart
+++ lib/notes/after_ui.dart
@@ -1,6 +1,12 @@
 // بعد ما أنشأت ال Ui وبلشت بال logic 
 //  لازم انتبه ان التطبيق بيشتغل ع ال divice يعني localy بدون نت 
-// حنستعمل divo يلي بتسبه بتعاملها التعامل مع ال firebase  
+// حنستعمل hive<package> يلي بتسبه بتعاملها التعامل مع ال firebase  
 // هي اسرع من ال mySql  وال mySql بدا يكون عندك خلفية ع squal 
 //---------------------------------------------------------------------
 // هلق لازم نبلش بال model 
+// فيك تستعمل Pubspec Assist  بشريط البحث ثم تضيف ال  package التي انت بحاجتها 
+// ال hive :  هي يلي لح استعملها لل local data pase فيك تستعملها في dart||flutter 
+// hive_flutter : هي بس extention من  hive بتخليك تشتغل ل  flutter 
+// بس لازم يكون عندك التنتين موجودين 
+// build_runner : هي بحطها بال dev_dependinces هي بتعمل generate code وانا بدي 
+// generate code ل flutter لهيك أنا بستعمل hive_generator
