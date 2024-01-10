7f668da1b3e46bb877aa88b56214919663be725c
Abdullah537-studio <112731731+Abdullah537-studio@users.noreply.github.com>
2024-01-10 15:31:54 +0300

    add addNote method in cubit

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,10 +1,23 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_to_do_list/Widget/Constants.dart';
 import 'package:flutter_to_do_list/models/note_model.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:meta/meta.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
-  addNote(NoteModels noteModel) {}
+  addNote(NoteModels note) async {
+    emit(AddNoteLoading());
+    // هون محطوط ال type dynamic ومعرفو بحرف يلي هو E مشان تبدلو بال model يلي عندك
+    // generic
+    try {
+      var notebox = Hive.box<NoteModels>(kNoteBox);
+      emit(AddNoteSuccess());
+      await notebox.add(note);
+    } catch (e) {
+      AddNoteFailure(erroreMessage: e.toString());
+    }
+  }
 }
